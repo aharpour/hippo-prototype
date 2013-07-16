9c5c2949a7213a13a81b36d7f93be413b0421f3c
aharpour <aharpour@7f5f0461-17b0-4ad1-ab5a-3c6b11fe03dc>
2013-07-16 13:25:08 +0000

    Better support for labels.

---
+++ trunk/frontend/src/main/java/com/tdclighthouse/prototype/componentsinfo/LabelsInfo.java
@@ -0,0 +1,13 @@
+package com.tdclighthouse.prototype.componentsinfo;
+
+import org.hippoecm.hst.core.parameters.Parameter;
+/**
+ * @author Ebrahim Aharpour
+ *
+ */
+public interface LabelsInfo {
+	
+	@Parameter(name = "labelPaths", description = "A comma separated list of paths to selection dropdowns", defaultValue = "")
+	public String getLabelPaths();
+
+}

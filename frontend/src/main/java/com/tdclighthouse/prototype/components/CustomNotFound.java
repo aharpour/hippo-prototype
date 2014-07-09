fba3efac0dd77fe27f0b11078bfe72d698aea64e
aharpour <aharpour@7f5f0461-17b0-4ad1-ab5a-3c6b11fe03dc>
2014-07-09 08:17:34 +0000

    CustomNotFound implementation was modified because it was using getContentBeanViaParameters and this method first looks at the site map item for the parameter and then the page configuration and that behavior is not desirable in this very particular case.

--- frontend/src/main/java/com/tdclighthouse/prototype/components/CustomNotFound.java
+++ frontend/src/main/java/com/tdclighthouse/prototype/components/CustomNotFound.java
@@ -3,23 +3,24 @@ package com.tdclighthouse.prototype.components;
 import javax.servlet.http.HttpServletResponse;
 
 import org.hippoecm.hst.component.support.bean.BaseHstComponent;
-import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstComponentException;
 import org.hippoecm.hst.core.component.HstRequest;
 import org.hippoecm.hst.core.component.HstResponse;
 import org.hippoecm.hst.core.parameters.ParametersInfo;
 
 import com.tdclighthouse.prototype.componentsinfo.ContentBeanPathInfo;
-import com.tdclighthouse.prototype.utils.BeanUtils;
 import com.tdclighthouse.prototype.utils.Constants.AttributesConstants;
 
 @ParametersInfo(type = ContentBeanPathInfo.class)
 public class CustomNotFound extends BaseHstComponent {
     @Override
     public void doBeforeRender(HstRequest request, HstResponse response) throws HstComponentException {
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-        ContentBeanPathInfo componentParametersInfo = getComponentParametersInfo(request);
-        HippoBean bean = BeanUtils.getContentBeanViaParameters(request, componentParametersInfo);
-        request.setAttribute(AttributesConstants.DOCUMENT, bean);
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            ContentBeanPathInfo componentParametersInfo = getComponentParametersInfo(request);
+            String contentBeanPath = componentParametersInfo.getContentBeanPath();
+            if (contentBeanPath != null) {
+                Object bean = request.getRequestContext().getSiteContentBaseBean().getBean(contentBeanPath);
+                request.setAttribute(AttributesConstants.DOCUMENT, bean);
+            }
     }
 }

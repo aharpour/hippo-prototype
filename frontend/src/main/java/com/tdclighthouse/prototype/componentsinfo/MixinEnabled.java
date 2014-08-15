a940923f69472782b1ac7a60b71d7da651e2ae7a
aharpour <aharpour@7f5f0461-17b0-4ad1-ab5a-3c6b11fe03dc>
2014-08-15 11:02:48 +0000

    typo in MixinEnabled interface was fixed.

--- frontend/src/main/java/com/tdclighthouse/prototype/componentsinfo/MixinEnabled.java
+++ frontend/src/main/java/com/tdclighthouse/prototype/componentsinfo/MixinEnabled.java
@@ -4,7 +4,7 @@ import org.hippoecm.hst.core.parameters.Parameter;
 
 public interface MixinEnabled {
     
-    @Parameter(name = "tryToUserMixin", displayName = "Try to use mixin", defaultValue = "off")
+    @Parameter(name = "tryToUseMixin", displayName = "Try to use mixin", defaultValue = "off")
     public Boolean getUseMixin();
     
 }

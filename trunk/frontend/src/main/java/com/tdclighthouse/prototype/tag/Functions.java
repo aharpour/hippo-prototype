e0bcef830f871a0df172c0457d9ad1bd46b6c026
aharpour <aharpour@7f5f0461-17b0-4ad1-ab5a-3c6b11fe03dc>
2014-09-30 15:33:00 +0000

    a new jsp function was added 'removeListByCategory'. it is useful for guided drill down faced navigation.

--- trunk/frontend/src/main/java/com/tdclighthouse/prototype/tag/Functions.java
+++ trunk/frontend/src/main/java/com/tdclighthouse/prototype/tag/Functions.java
@@ -1,6 +1,10 @@
 package com.tdclighthouse.prototype.tag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hippoecm.hst.content.beans.standard.HippoBean;
+import org.hippoecm.hst.content.beans.standard.facetnavigation.HippoFacetSubNavigation;
 
 import com.tdclighthouse.prototype.beans.Page;
 
@@ -15,6 +19,18 @@ public class Functions {
             result = true;
         }
         return result;
+    }
+
+    public static List<HippoFacetSubNavigation> removeListByCategory(String category, String childCategory,
+            HippoFacetSubNavigation item) {
+        List<HippoFacetSubNavigation> removeList = new ArrayList<HippoFacetSubNavigation>();
+        for (HippoFacetSubNavigation ancestors : item.getAncestorsAndSelf()) {
+            String actuallyCategory = ancestors.getParentBean().getName();
+            if (category.equals(actuallyCategory) || childCategory.equals(actuallyCategory)) {
+                removeList.add(ancestors);
+            }
+        }
+        return removeList;
     }
 
 }

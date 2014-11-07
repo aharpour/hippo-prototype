b5c0704d0b1976885a50ca5d169d77b58f91a5d4
aharpour <aharpour@7f5f0461-17b0-4ad1-ab5a-3c6b11fe03dc>
2014-11-07 17:16:36 +0000

    replacing binary importer with package importer.

--- trunk/plugin-support/src/main/java/com/tdclighthouse/prototype/standalone/importer/PackageImporter.java
+++ trunk/plugin-support/src/main/java/com/tdclighthouse/prototype/standalone/importer/PackageImporter.java
@@ -1,6 +1,8 @@
-package com.tdclighthouse.prototype.standalone.binaryimporter;
+package com.tdclighthouse.prototype.standalone.importer;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.zip.ZipFile;
 
@@ -12,28 +14,37 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.tdclighthouse.prototype.services.BinaryImportService;
 import com.tdclighthouse.prototype.utils.FileUtils;
 import com.tdclighthouse.prototype.utils.PluginConstants;
+import com.tdclighthouse.prototype.utils.RuntimeRepositoryException;
 import com.tdclighthouse.prototype.utils.exceptions.RuntimeIOException;
 
-public class App {
+public class PackageImporter {
 
     private static final String EXTENSION_ZIP = ".zip";
     private final String importFolder;
     public static final ClassPathXmlApplicationContext APPLICATION_CONTEXT = new ClassPathXmlApplicationContext(
-            App.class.getPackage().getName().replace('.', '/') + "/binary-importer-context.xml");
+            PackageImporter.class.getPackage().getName().replace('.', '/') + "/importer-context.xml");
 
-    public App(String importFolder) {
+    private FilenameFilter folderFilter = new FilenameFilter() {
+
+        @Override
+        public boolean accept(File dir, String name) {
+            return dir.isDirectory();
+        }
+    };
+
+    public PackageImporter(String importFolder) {
         this.importFolder = importFolder;
     }
 
-    public static void main(String[] args) throws RepositoryException {
+    public static void main(String[] args) {
         if (args.length != 1) {
             throw new IllegalArgumentException(
                     "This application requires a single command line argument pointing to resource you want to import.");
         }
         File resource = new File(args[0]);
         if (resource.exists()) {
-            App app = APPLICATION_CONTEXT.getBean(App.class);
-            app.importBinaries(resource);
+            PackageImporter app = APPLICATION_CONTEXT.getBean(PackageImporter.class);
+            app.importFileOrFolder(resource);
         }
     }
 
@@ -41,18 +52,17 @@ public class App {
         return importFolder;
     }
 
-    private void importBinaries(File resource) throws RepositoryException {
+    private void importFileOrFolder(File resource) {
         try {
-            BinaryImportService binaryImportService = APPLICATION_CONTEXT.getBean(BinaryImportService.class);
             if (resource.isDirectory()) {
-                binaryImportService.migrateFolder(resource, importFolder);
+                bulkImport(resource);
             } else {
                 if (resource.getName().endsWith(EXTENSION_ZIP)) {
                     ZipFile zipFile = new ZipFile(resource);
                     File temp = createTempFolder(zipFile);
                     try {
                         FileUtils.unzip(zipFile, temp);
-                        binaryImportService.migrateFolder(temp, importFolder);
+                        bulkImport(temp);
                     } finally {
                         FileUtils.forcefulDeletion(temp);
                     }
@@ -60,8 +70,31 @@ public class App {
             }
         } catch (IOException e) {
             throw new RuntimeIOException(e);
+        } catch (RepositoryException e) {
+            throw new RuntimeRepositoryException(e);
         }
+    }
 
+    private void bulkImport(File resource) throws RepositoryException {
+        File[] folders = resource.listFiles(folderFilter);
+        for (File folder : folders) {
+            importItem(folder);
+        }
+    }
+
+    private void importItem(File folder) throws RepositoryException {
+        BinaryImportService binaryImportService = APPLICATION_CONTEXT.getBean(BinaryImportService.class);
+        File[] binaries = folder.listFiles(new FilterFolderByName("binaries"));
+        if (binaries.length > 0) {
+            binaryImportService.migrateFolder(binaries[0], importFolder);
+        }
+        
+        File[] images = folder.listFiles(new FilterFolderByName("images"));
+        if (images.length > 0) {
+            binaryImportService.migrateFolder(images[0], importFolder);
+        }
+        
+        folder.listFiles(new FilterFolderByName("xmls"));
     }
 
     private File createTempFolder(ZipFile zipFile) throws IOException {
@@ -76,5 +109,18 @@ public class App {
 
     private String getSimpleFileName(ZipFile zipFile) {
         return FilenameUtils.removeExtension(FileUtils.getFileName(zipFile.getName()));
+    }
+
+    private class FilterFolderByName implements FileFilter {
+        private final String folderName;
+
+        public FilterFolderByName(String folderName) {
+            this.folderName = folderName;
+        }
+
+        @Override
+        public boolean accept(File file) {
+            return file.isDirectory() && folderName.equalsIgnoreCase(file.getName());
+        }
     }
 }

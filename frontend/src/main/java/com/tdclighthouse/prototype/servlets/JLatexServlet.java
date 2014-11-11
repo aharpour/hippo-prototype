45266351c2cf9a0a95900cbd85d2a4fda849a6b6
aharpour <aharpour@7f5f0461-17b0-4ad1-ab5a-3c6b11fe03dc>
2014-11-11 13:51:13 +0000

    QRCode servlet was added.

--- frontend/src/main/java/com/tdclighthouse/prototype/servlets/JLatexServlet.java
+++ frontend/src/main/java/com/tdclighthouse/prototype/servlets/JLatexServlet.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.tdclighthouse.prototype.latex;
+package com.tdclighthouse.prototype.servlets;
 
 import java.awt.Color;
 import java.awt.Graphics2D;

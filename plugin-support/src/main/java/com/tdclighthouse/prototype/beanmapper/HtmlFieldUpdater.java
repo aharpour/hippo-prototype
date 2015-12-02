9851a5596ab567861e91e689fb608309adbfa992
Ebrahim aharpour <ebrahim@openweb.nl>
2015-12-02 16:41:29 +0100

    changing the link regex to handle elements with in the a tag.

--- plugin-support/src/main/java/com/tdclighthouse/prototype/beanmapper/HtmlFieldUpdater.java
+++ plugin-support/src/main/java/com/tdclighthouse/prototype/beanmapper/HtmlFieldUpdater.java
@@ -38,9 +38,9 @@ public class HtmlFieldUpdater implements DynamicNodeUpdater {
     // .*src="([^'|^"]*)".*
     private static final Pattern IMAGE_SOURCE_PATTERN = Pattern.compile(".*src=\"([^'|^\"]*)\".*");
 
-    // <a\s[^>]*href=["|']([^"^']*)["|'][^>]*>([^<]*)</a>
+    // <a\s[^>]*href=["|']([^"^']*)["|'][^>]*>((?!</a).*)</a>
     private static final Pattern LINK_PATTERN = Pattern
-            .compile("<a\\s[^>]*href=[\"|']([^\"^']*)[\"|'][^>]*>([^<]*)</a>");
+            .compile("<a\\s[^>]*href=[\"|']([^\"^']*)[\"|'][^>]*>((?!</a).*)</a>");
 
     @Override
     public DynamicNode update(DynamicNode dynamicNode) throws ImportException {
